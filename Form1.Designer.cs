a2c86bdaf55053e8d58152a2d1ad5a9634656bf7
Gabriel Dorotheo <gbldorotheo@hotmail.com>
2023-01-09 05:40:47 -0300

    Gerador de senhas

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace GeradorDeSenha
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tb1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tb2 = new System.Windows.Forms.TextBox();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.Location = new System.Drawing.Point(12, 85);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(127, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Tamanho Da Senha";
+			// 
+			// tb1
+			// 
+			this.tb1.Location = new System.Drawing.Point(145, 84);
+			this.tb1.Name = "tb1";
+			this.tb1.Size = new System.Drawing.Size(100, 23);
+			this.tb1.TabIndex = 1;
+			this.tb1.Text = "8";
+			this.tb1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb1_KeyPress);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(11, 124);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(128, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Gerar senha";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tb2
+			// 
+			this.tb2.Location = new System.Drawing.Point(12, 153);
+			this.tb2.Multiline = true;
+			this.tb2.Name = "tb2";
+			this.tb2.Size = new System.Drawing.Size(228, 66);
+			this.tb2.TabIndex = 3;
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label2.Location = new System.Drawing.Point(44, 20);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(226, 32);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Gerador de senhas";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(320, 231);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.tb2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.tb1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Label label1;
+		private TextBox tb1;
+		private Button button1;
+		private TextBox tb2;
+		private NotifyIcon notifyIcon1;
+		private Label label2;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+namespace GeradorDeSenha
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void tb1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			e.Handled = (!char.IsDigit(e.KeyChar)) && (!char.IsControl(e.KeyChar));
+		}
+
+		private void ValidaTamanhoSenha()
+		{
+			int TamanhoDaSenha = 8;
+			if (!string.IsNullOrWhiteSpace(tb1.Text))
+			{
+				TamanhoDaSenha = Convert.ToInt32(tb1.Text);
+				if (TamanhoDaSenha > 20)
+				{
+					TamanhoDaSenha = 20;
+					tb1.Text = "20";
+				}
+				if (TamanhoDaSenha < 8)
+				{
+					TamanhoDaSenha = 8;
+					tb1.Text = "8";
+				}
+			}
+			else
+			{
+				tb1.Text = "8";
+			}
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (int.Parse(tb1.Text) < 8 || int.Parse(tb1.Text) > 20)
+			{
+				tb2.Text = "O tamanho da senha deve ser maior que 8 e menor que 20.";
+			}
+			else
+			{
+				ValidaTamanhoSenha();
+				string caracteresPermitidos = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
+				char[] chars = new char[int.Parse(tb1.Text)];
+				Random rd = new Random();
+				for (int i = 0; i < int.Parse(tb1.Text); i++)
+				{
+					chars[i] = caracteresPermitidos[rd.Next(0, caracteresPermitidos.Length)];
+				}
+				tb2.Text = new string(chars);
+			}	
+		}
+
+	}
+}
